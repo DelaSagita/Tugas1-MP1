1c50afb061fb6f6069124d81086e8318e9607d76
HerdiantiNorrlita <90747210+DelaSagita@users.noreply.github.com>
2021-09-27 09:46:17 +0700

    Tugas1-MP1

---
+++ Instagramku/app/src/main/java/com/dela/instagramku/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dela.instagramku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
